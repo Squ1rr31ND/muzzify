a7232555e541e0780d80f58a9f571fd420383fdb
Squ1rr31ND <Squ1rr31ND@gmail.com>
2026-09-06 08:20:42 +0300

    feat: register API Client in service locator

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import '../network/api_client.dart';
 import '../network/dio_client.dart';
 
 final locator = GetIt.instance;
@@ -8,5 +9,9 @@ final locator = GetIt.instance;
 void setupLocator() {
   locator.registerLazySingleton<Dio>(() {
     return DioClient().dio;
+  });
+
+  locator.registerLazySingleton<ApiClient>(() {
+    return ApiClient(dio: locator());
   });
 }
