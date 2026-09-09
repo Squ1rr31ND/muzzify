f5b92e458200564f3cb6a53231a533f649936456
Squ1rr31ND <Squ1rr31ND@gmail.com>
2026-09-09 22:42:47 +0300

    chore(core): register token storage

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,13 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../features/auth/di/auth_di.dart';
 import '../network/api_client.dart';
 import '../network/dio_client.dart';
+import '../storage/secure_token_storage.dart';
+import '../storage/token_storage.dart';
 
 final locator = GetIt.instance;
 
 void setupLocator() {
+  locator.registerLazySingleton<FlutterSecureStorage>(() {
+    return const FlutterSecureStorage();
+  });
+
+  locator.registerLazySingleton<TokenStorage>(() {
+    return SecureTokenStorage(secureStorage: locator());
+  });
+
   locator.registerLazySingleton<Dio>(() {
     return DioClient().dio;
   });
